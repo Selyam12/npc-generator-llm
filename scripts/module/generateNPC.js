96a3317f00ad6dadb441489e9bd124049b0c5dbf
Selyam <coollibri.unpaved420@passmail.net>
2024-06-15 15:36:25 +0200

    remove commented code

--- scripts/module/generateNPC.js
+++ scripts/module/generateNPC.js
@@ -29,11 +29,6 @@ export class npcGenGPTGenerateNPC extends Application {
         
         data.type =  { value: "type", label: `npc-generator-llm.dialog.type.label`, option: npcGenGPTLib.getDialogOptions('type',false) };
         data.category =   NPCFactory.createInstance('commoner').getDialogCategories();
-        //const categories = npcGenGPTLib.getDialogCategories(npcGenGPTDataStructure.categoryList);
-        // data.category = categories.map(category => {
-        //     const arg = (category.value === 'subtype') ? 'commoner' : category.value;
-        //     return { ...category, option: npcGenGPTLib.getDialogOptions(arg, (arg !== 'type' && arg !== 'cr')) };
-        // });
         return data;
     }
 
@@ -42,16 +37,6 @@ export class npcGenGPTGenerateNPC extends Application {
         const npc = NPCFactory.createInstance(npcType);
         
         npc.setHtmlElements(this.element);
-        // const generateOptions = (data, random) => {
-        //     return npc.getDialogOptions(data, random).map(subtype => {
-        //         if (subtype.translate) subtype.label = game.i18n.localize(subtype.label);
-        //         return `<option value="${subtype.value}">${subtype.label}</option>`;
-        //     }).join('');
-        // };
-        // const label = game.i18n.localize(`npc-generator-llm.dialog.subtype.${((npcType === 'npc') ? 'class' : (npcType === 'monster') ?'type':'label')}`);
-        // this.element.find("label[for='subtype']").text(`${label}:`);
-        // this.element.find("#subtype").html(generateOptions(npcType, true));
-        // this.element.find("#cr").html(generateOptions('cr', npcType === 'npc'));
     }
 
     async initGeneration() {
@@ -95,136 +80,5 @@ export class npcGenGPTGenerateNPC extends Application {
        npc.parseHTML(this.element);
 
        return npc;
-
-       /*  npcGenGPTDataStructure.categoryList.forEach(category => {
-            const dialogCategory = this.element.find(`#${category}`);
-            this.data.details[category] = npcGenGPTLib.getSelectedOption(dialogCategory);
-        });
-        const { cr, race, type, subtype } = this.data.details;
-
-        subtype.value = (type.value === 'commoner') ? type.value : subtype.value;
-        this.data.details.optionalName = this.element.find('#name').val();
-        this.data.details.sheet = (type.value === 'commoner') ? 'npc-generator-llm.dialog.subtype.label' : (type.value === 'monster')?'npc-generator-llm.dialog.subtype.type':'npc-generator-llm.dialog.subtype.class';
-        this.data.abilities = this.generateNpcAbilities(subtype.value, cr.value);
-        this.data.attributes = this.generateNpcAttributes(race.value, subtype.value, cr.value);
-        this.data.skills = this.generateNpcSkills(race.value, subtype.value);
-        this.data.traits = this.generateNpcTraits(race.value, subtype.value);
-        this.data.currency = npcGenGPTLib.getNpcCurrency(cr.value); */
     }
-
-    // initQuery() {
-    //     const { optionalName, gender, race, subtype, alignment } = this.data.details;
-    //     let options = `${gender.label}, ${race.label}, ${subtype.label}, ${alignment.label}`;
-    //     if (optionalName) options = `(${game.i18n.localize("npc-generator-llm.query.name")}: ${optionalName}) ${options}`; 
-    //     return npcGenGPTDataStructure.getGenerateQueryTemplate(options)
-    // }
-
-    // mergeGptData(gptData) {
-    //     const { name: gptName, spells, items, appearance, background, roleplaying, readaloud } = gptData;
-    //     this.data.name = gptName;
-    //     this.data.spells = spells;
-    //     this.data.items = items;
-    //     this.data.details = {
-    //         ...this.data.details,
-    //         source: "NPC Generator (GPT)",
-    //         biography: {
-    //             appearance: appearance,
-    //             background: background,
-    //             roleplaying: roleplaying,
-    //             readaloud: readaloud
-    //         }
-    //     };
-    // }
-
-    // async createNPC() {
-    //     try {
-    //         const { abilities, attributes, details, name, skills, traits, currency } = this.data;
-    //         const fakeAlign = (game.settings.get(COSTANTS.MODULE_ID, "hideAlignment")) ? game.i18n.localize("npc-generator-llm.sheet.unknown") : details.alignment.label;
-    //         const bioContent = await npcGenGPTLib.getTemplateStructure(COSTANTS.TEMPLATE.SHEET, this.data);
-
-    //         const npc_ = await Actor.create({ name: name, type: "npc" });
-    //         await npc_.update({
-    //             system: {
-    //                 details: {
-    //                     source: details.source,
-    //                     cr: details.cr.value,
-    //                     alignment: fakeAlign,
-    //                     race: details.race.label,
-    //                     biography: { value: bioContent },
-    //                     type:  { value: 'custom', custom: details.race.label }
-    //                 },
-    //                 traits: { size: traits.size, languages: { value: traits.languages } },
-    //                 abilities: abilities,
-    //                 attributes: {
-    //                     hp: attributes.hp,
-    //                     'ac.value': attributes.ac,
-    //                     movement: attributes.movement,
-    //                     senses: attributes.senses,
-    //                     spellcasting: attributes.spellcasting
-    //                 },
-    //                 skills: skills,
-    //                 currency: currency
-    //             }
-    //         });
-
-    //         let comp = npcGenGPTLib.getSettingsPacks();
-    //         npcGenGPTLib.addItemstoNpc(npc_, comp.items, this.data.items);
-    //         npcGenGPTLib.addItemstoNpc(npc_, comp.spells, this.data.spells);
-
-    //         npc_.sheet.render(true);
-
-    //         this.close();
-    //         ui.notifications.info(`${COSTANTS.LOG_PREFIX} ${game.i18n.format("npc-generator-llm.status.done", { npcName: name })}`);
-    //     } catch (error) {
-    //         console.error(`${COSTANTS.LOG_PREFIX} Error during NPC creation:`, error);
-    //         ui.notifications.error(`${COSTANTS.LOG_PREFIX} ${game.i18n.localize("npc-generator-llm.status.error3")}`);
-    //     }
-    // }
-
-    // generateNpcAbilities(npcSubtype, npcCR) {
-    //     const npcStats = npcGenGPTDataStructure.subtypeData[npcSubtype];
-    //     const profAbilities = (npcSubtype === 'commoner'||'monster')
-    //         ? npcGenGPTLib.getRandomFromPool(npcStats.save.pool, npcStats.save.max)
-    //         : npcStats.save;
-    //     const npcAbilities = npcGenGPTLib.getNpcAbilities(profAbilities);
-    //     return npcGenGPTLib.scaleAbilities(npcAbilities, npcCR)
-    // }
-
-    // generateNpcAttributes(npcRace, npcSubtype, npcCR) {
-    //     const raceData = npcGenGPTDataStructure.raceData[npcRace];
-    //     const subtypeData = npcGenGPTDataStructure.subtypeData[npcSubtype];
-    //     const measureUnits = game.settings.get(COSTANTS.MODULE_ID, "movementUnits") ? 'm' : 'ft';
-    //     return {
-    //         hp: npcGenGPTLib.getNpcHp(npcCR, this.data.abilities.con.value, raceData.size),
-    //         ac: npcGenGPTLib.getNpcAC(npcCR),
-    //         spellcasting: subtypeData[npcSubtype]?.spellcasting && 'int',
-    //         movement: { ...((measureUnits === 'm') ? npcGenGPTLib.convertToMeters(raceData.movement) : raceData.movement), units: measureUnits },
-    //         senses: { ...((measureUnits === 'm') ? npcGenGPTLib.convertToMeters(raceData.senses) : raceData.senses), units: measureUnits }
-    //     }
-    // }
-
-    // generateNpcSkills(npcRace, npcSubtype) {
-    //     const { pool: defaultPool, max } = npcGenGPTDataStructure.subtypeData[npcSubtype].skills;
-    //     const pool = (npcRace === 'elf' || npcRace === 'drow')
-    //         ? npcGenGPTLib.getRandomFromPool(defaultPool.filter(skill => skill !== 'prc'), max).concat('prc')
-    //         : npcGenGPTLib.getRandomFromPool(defaultPool, max);
-
-    //     return pool.reduce((acc, el) => {
-    //         acc[el] = { value: 1, ability: npcGenGPTLib.getSkillAbility(el) };
-    //         return acc;
-    //     }, {});
-    // }
-
-    // generateNpcTraits(npcRace, npcSubtype) {
-    //     const languages = (npcGenGPTDataStructure.raceData[npcRace].lang || []).slice();
-    //     const subtypeLanguages = (npcGenGPTDataStructure.subtypeData[npcSubtype].lang || []).slice();
-    //     for (const subLang of subtypeLanguages) if (!languages.includes(subLang)) languages.push(subLang);
-    //     if (npcRace === 'human' || npcRace === 'halfelf') {
-    //         languages.push(npcGenGPTLib.getRandomFromPool(npcGenGPTDataStructure.languagesList.filter(lang => !languages.includes(lang)), 1)[0]);
-    //     }
-    //     return {
-    //         languages: languages,
-    //         size: npcGenGPTDataStructure.raceData[npcRace].size
-    //     }
-    // }
 }
